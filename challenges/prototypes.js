d3082a9919664a76e3163d0fd5015c3244b1b419
anthony <anthony24.kwhomes@gmail.com>
2019-03-08 09:05:04 -0800

    Initalizing Challenge

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,7 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(length, width, height);
 
 
 /* == Step 2: Volume Method ==
